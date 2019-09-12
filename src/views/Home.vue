aa7d31d53d7c2b38b2e22a08e0f88cc274298670
Jake <JayHay28324@gmail.com>
2019-09-12 06:13:20 -0400

    Complete

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,89 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home-view-contaier">
+    <h1>Adopt a new best friend</h1>
+    <h2>There are {{ animalsCount }} available!</h2>
+    <button class="btn btn-primary" @click="togglePetForm">Add New Pet</button>
+
+    <b-form @submit.prevent="handleSubmit" v-if="showPetForm">
+      <b-form-group id="input-group-1" label="Pet's Name:" label-for="input-1">
+        <b-form-input
+          id="input-1"
+          v-model="formData.name"
+          required
+          placeholder="Enter name"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Species:" label-for="input-2">
+        <b-form-select
+          id="input-2"
+          v-model="formData.species"
+          :options="['Cats','Dogs']"
+          required
+        ></b-form-select>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Pet's Age:" label-for="input-3">
+        <b-form-input
+          id="input-3"
+          type="number"
+          v-model="formData.age"
+          required
+          placeholder="Enter age"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+    </b-form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mapActions, mapGetters } from 'vuex'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+  data () {
+    return {
+      showPetForm: false,
+      formData: {
+        name: '',
+        age: 0,
+        species: null
+      }
+    }
+  },
+  methods: {
+    ...mapActions([
+      'addPet'
+    ]),
+    togglePetForm () {
+      this.showPetForm = !this.showPetForm
+    },
+    computed: {
+      ...mapGetters([
+        'animalsCount'
+      ])
+    },
+    handleSubmit () {
+      const { species, age, name } = this.formData
+
+      const payload = {
+        species,
+        pet: {
+          name,
+          age
+        }
+      }
+      this.addPet(payload)
+
+      this.formData = {
+        name: '',
+        age: 0,
+        species: null
+      }
+    }
+  },
+  components: {}
 }
 </script>
---
+++ src/views/Pet.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>{{ animal.name }}</h1>
+    <h3>Age: {{ animal.age }} years old</h3>
+    <p>Breed: {{ animal.breed }}</p>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data () {
+    return {
+      animal: {}
+    }
+  },
+  computed: {
+    ...mapState([
+      'cats',
+      'dogs'
+    ])
+  },
+  mounted () {
+    const animal = this[this.$route.params.species][this.$route.params.id]
+    this.animal = animal
+  }
+}
+</script>
